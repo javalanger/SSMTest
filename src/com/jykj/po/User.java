13094f9527567491c62bb4f35cc00b076741c764
cp <cp@163.com>
2020-07-30 16:31:16 +0800

    新增User po

---
+++ src/com/jykj/po/User.java
@@ -0,0 +1,26 @@
+package com.jykj.po;
+
+public class User {
+	private Integer id;
+	private Integer gender;
+	private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getGender() {
+		return gender;
+	}
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
